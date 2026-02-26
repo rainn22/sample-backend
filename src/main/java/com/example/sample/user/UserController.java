830dcef19ffe9d8590e79483ec3d86a7f5ab8235
rain <sorany.non@student.cadt.edu.kh>
2026-02-26 09:52:16 +0700

    add workflow

--- src/main/java/com/example/sample/user/UserController.java
+++ src/main/java/com/example/sample/user/UserController.java
@@ -18,8 +18,37 @@ public class UserController {
         return repo.findAll();
     }
 
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
     @PostMapping
     public User create(@RequestBody User user) {
         return repo.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id,
+                           @RequestBody User updatedUser) {
+
+        return repo.findById(id)
+                .map(user -> {
+                    user.setName(updatedUser.getName());
+                    return repo.save(user);
+                })
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+
+        if (!repo.existsById(id)) {
+            throw new RuntimeException("User not found");
+        }
+
+        repo.deleteById(id);
+        return "User deleted successfully";
     }
 }
